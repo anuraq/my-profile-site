88f5836ac92b3af617d4511bbd2896835141cec7
unknown <yoyoanusingh27@gmail.com>
2020-07-13 23:56:50 +0530

    list-view

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import  './style.css'
+import React, { Component } from 'react'
+import './style.css'
 
-function Project() {
-    return (
-        <>  
+export class Project extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {p_list: ["Socket Stream", "Tetris" , "Calculator", "B", "Socket Stream", "Tetris" , "Calculator", "B", "Socket Stream", "Tetris" , "Calculator", "B","Socket Stream", "Tetris" , "Calculator", "B"]};
+    }
+    
+    render() {
+        return (
             <div className="project-container">
                 <div className="contact-title">Projects</div>
-                <div className="project-design"></div>
+                <div className="project-design">
+                    <div className="project-list">
+                      {this.state.p_list.map((x,y) => <ProjectName name={x} key={y} />)}  
+                    </div>
+                    <div className="project-info"></div>
+                </div>
             </div>
-        </>
+        )
+    }
+}
+
+function ProjectName(props) {
+    return (
+    <div className="project-name">{props.name}</div>
     )
 }
+
 
 export default Project
