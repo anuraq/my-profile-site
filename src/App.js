5d02ecc6f52069db93f5baaa08659a854b28a524
Kumar Anurag <yoyoanusingh27@gmail.com>
2021-09-30 20:21:48 +0530

    :')

--- src/App.js
+++ src/App.js
@@ -30,9 +30,6 @@ export class App extends Component {
         if(window.scrollY > 40) {
             this.setState({showHidden:true});
         }
-        else {
-            this.setState({showHidden:false});
-        }
     }
 
     render() {
